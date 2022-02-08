c10dfa3f47080518aff48b539f9f546961e97cf3
Enzo Noda <enzo.noda@gmail.com>
2022-02-07 23:44:55 -0300

    first commit

---
+++ src/Components/Conta/index.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Icone, Box, Button, Detail, Saldo, IconeTema } from "../UI";
+import styled from "styled-components";
+import privado from "../../assets/images/privado.svg";
+import olho_icone from "../../assets/images/olho.svg";
+import dinheiro from "../../assets/images/dinheiro.svg";
+
+const IconeMargin = styled(Icone)`
+  margin-top:2px;
+`
+
+const Conta = () => {
+  const [toggleState, untoggle] = useState(true);
+
+  const toggleHandler = () => {
+    untoggle((toggleState) => !toggleState);
+  };
+
+  return (
+    <Box>
+      <h2>Conta</h2>
+      <div style={{ fontSize: "26px", padding: "20px 0" }}>
+        Saldo disponível{" "}
+        <span>
+          <IconeTema src={dinheiro} alt="Ícone Saldo" />
+        </span>
+        {toggleState ? (
+          <Saldo>
+            <Detail className="detalhe">R$</Detail> 0,00{" "}
+          </Saldo>
+        ) : null}
+      </div>
+
+      <Button className="btn" onClick={toggleHandler}>
+        <IconeMargin
+          src={toggleState ? privado : olho_icone}
+          alt="Privacidade do Saldo"
+        />
+      </Button>
+    </Box>
+  );
+};
+
+export default Conta;
